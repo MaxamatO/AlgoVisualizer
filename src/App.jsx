78d099d1c863d5a1a57c2c14147ed652402eabde
Maksymilian Pierchała <maksdfg123@gmail.com>
2022-11-20 21:54:42 +0100

    just a save

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,28 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-
+  
   const [numbers, setNumbers] = useState([]);
   const [isSorted, setIsSorted] = useState(false);
 
   const refreshApp = () => {
     setIsSorted(isSorted);
+    setNumbers(numbers)
   }
 
   useEffect(() => {
-    setNumbers(numbers)
-  }, [])
+    refreshApp();
+  }, [numbers])
 
   const generateNew = () => {
-    setIsSorted(false);
-    return generateRandomNumbers();
+    let rects = document.getElementsByClassName("rect");
+        for(let r=0; r<rects.length; r++){
+          rects[r].style.borderColor = "blue";
+        }
+        setIsSorted(false)
+    return setNumbers(generateRandomNumbers());
   }
-
+  
   const shuffle = (array) => {
     var tmp, current, top = array.length;
     if(top) while(--top) {
@@ -31,39 +36,45 @@ function App() {
   }
 
   const generateRandomNumbers = () => {
-    for (var a=[],i=0;i<200;++i) a[i]=i; 
+    for (var a=[],i=0;i<50;++i) a[i]=i; 
     return shuffle(a);
   }
 
   const bubblesort = (nums) => {
-      setIsSorted(true);
-      let iteration = 0;
+      let i = 0;
       let inMemory;
-      while (iteration<nums.length-1){
-          for(let i=0; i<nums.length-1; i++){
-              console.log("Comparing: " + nums[i] +"-"+nums[i+1]);
-              if(nums[i]>nums[i+1]){
-              inMemory = nums[i+1]
-              nums[i+1]=nums[i]
-              nums[i]=inMemory;
-              }
-          }
-          iteration = iteration+1;
-          setNumbers(nums)
+      for (var ids=[],x=0;x<nums.length;++x) ids[x]=x; 
+      for(i=nums.length; i>0; i--){
+          for(let j=0; j<i; j++){
+            if(nums[j]>nums[j+1]){
+              inMemory = nums[j+1]
+              nums[j+1]=nums[j]
+              nums[j]=inMemory;
+            }
+
+        }
+        let rects = document.getElementsByClassName("rect");
+        for(let r=0; r<rects.length; r++){
+          rects[r].style.borderColor = "red";
+        }
       }
-      return;
+      setNumbers(nums)
+      setIsSorted(true);
+      return numbers;
+
   }
   function BubbleSort(){
     if(!isSorted){
-      let array = generateNew();
-      return array.map((number) => {
+      let array = generateRandomNumbers();
+      return array.map((number, index) => {
         return (
-          <div className='rect' key={number} style={{height:number*3+"px"}}>
+          <div className='rect' id={index} key={number} style={{height:number*3+"px"}}>
           </div>
         )
       }
       );
     }
+    console.log(numbers);
     return(
       numbers.map((number) => {
         return (
@@ -78,7 +89,7 @@ function App() {
       <button onClick={() => {bubblesort(generateRandomNumbers())}}>Bubble sort</button>
       <button onClick={() => {generateNew()}}>Generate new</button>
       <div className='main-display'>
-        <BubbleSort/>
+        <BubbleSort></BubbleSort>
       </div>
     </div>
   );
