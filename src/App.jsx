2137c4087d010e83f1aeb5f02b8bb877e7cbd13e
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-02-01 20:37:06 +0100

    Remove merge sort

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,41 @@
-import './App.css';
-import { dobubblesort } from './BubbleSort';
-import React from 'react';
-import { useRef } from 'react';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import {getMergeSortAnimations} from './Mergesort'
-const PRIMARY_COLOR = 'turquoise';
+import "./App.css";
+import { dobubblesort } from "./BubbleSort";
+import React from "react";
+import { useRef } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { getMergeSortAnimations } from "./Mergesort";
+const PRIMARY_COLOR = "turquoise";
 
-
-const App =() => {
+const App = () => {
   const [numbers, setNumbers] = useState([]);
   let sorted = useRef(false);
 
   useEffect(() => {
     setNumbers(generateNew());
     // eslint-disable-next-line
-    }, []);
+  }, []);
 
-  const generateRandomNumbers = (min, max) =>{
+  const generateRandomNumbers = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
-  }
+  };
   const generateNew = () => {
     const array = [];
-    for(let i = 0; i<100; i++){
+    for (let i = 0; i < 100; i++) {
       array.push(generateRandomNumbers(2, 200));
     }
     return array;
-  }
+  };
 
-  const bubblesort = (numbers) =>{
-    if(sorted === true) return;   
+  const bubblesort = (numbers) => {
+    if (sorted === true) return;
     let animations = dobubblesort(numbers)[1];
     console.log(animations);
     const buttons = document.getElementsByTagName("button");
-    for(let i = 0; i<buttons.length; i++){
+    for (let i = 0; i < buttons.length; i++) {
       buttons[i].disabled = true;
     }
-    for(let i=0; i<animations.length; i++){
+    for (let i = 0; i < animations.length; i++) {
       const bars = document.getElementsByClassName("rect");
       const [firstBar, secondBar] = animations[i];
       setTimeout(() => {
@@ -50,20 +49,20 @@ const App =() => {
         setTimeout(() => {
           firstBarStyle.borderColor = PRIMARY_COLOR;
           secondBarStyle.borderColor = PRIMARY_COLOR;
-        }, 20)
-      }, i*20)
+        }, 20);
+      }, i * 20);
     }
-   setTimeout(() => {
-    for(let i = 0; i<buttons.length; i++){
-      buttons[i].disabled = false;
-    }
-    sorted = true;
-   }, animations.length*20)
-  }
+    setTimeout(() => {
+      for (let i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = false;
+      }
+      sorted = true;
+    }, animations.length * 20);
+  };
 
   const mergesort = (array) => {
     let animations = getMergeSortAnimations(array);
-    for(let i=0; i<animations.length; i++){
+    for (let i = 0; i < animations.length; i++) {
       const bars = document.getElementsByClassName("rect");
       const [firstBar, secondBar] = animations[i];
       setTimeout(() => {
@@ -77,11 +76,11 @@ const App =() => {
         setTimeout(() => {
           firstBarStyle.borderColor = PRIMARY_COLOR;
           secondBarStyle.borderColor = PRIMARY_COLOR;
-        }, 30)
-      }, i*30)
+        }, 30);
+      }, i * 30);
     }
     console.log(getMergeSortAnimations(array));
-  }
+  };
 
   // To implement
   // quicksort(){
@@ -89,26 +88,39 @@ const App =() => {
   //   console.log(array);
   // }
 
-  
   return (
-    <div className='main-layout'>
+    <div className="main-layout">
       <div className="menu">
-      <button onClick={() => {bubblesort(numbers)}}>Bubble Sort</button>
-      {/* <button disabled={true} onClick={() => {quicksort()}} >Quicksort</button> */}
-      <button onClick={() => {mergesort(numbers)}} >Merge Sort</button>
-      <button onClick={() => {setNumbers(generateNew())}} >New Array</button>
+        <button
+          onClick={() => {
+            bubblesort(numbers);
+          }}
+        >
+          Bubble Sort
+        </button>
+        {/* <button disabled={true} onClick={() => {quicksort()}} >Quicksort</button> */}
+        {/* <button onClick={() => {mergesort(numbers)}} >Merge Sort</button> */}
+        <button
+          onClick={() => {
+            setNumbers(generateNew());
+          }}
+        >
+          New Array
+        </button>
       </div>
-      <div className='main-display'>
-      {numbers.map((number, index) => {
-        return (
-          <div className='rect' key={index} style={{height:number*2+"px", borderColor: PRIMARY_COLOR}}>
-          </div>
-        ) 
-      })}
+      <div className="main-display">
+        {numbers.map((number, index) => {
+          return (
+            <div
+              className="rect"
+              key={index}
+              style={{ height: number * 2 + "px", borderColor: PRIMARY_COLOR }}
+            ></div>
+          );
+        })}
       </div>
     </div>
   );
-  
 };
 
 export default App;
